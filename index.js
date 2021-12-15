73a3c2b9562f155d6872bc68b6edccc18c9e534b
davidsonjose <davidsonjose313@gmail.com>
2021-12-15 09:43:09 -0800

    introduced ejs

--- index.js
+++ index.js
@@ -1,42 +1,61 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const {data} = require("./test");
+const { data } = require("./test");
+const expressLayout = require("express-ejs-layouts");
+
+// console.log(http.STATUS_CODES);
 
 //initialized app
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-let index1 = path.join(__dirname, "views/index.html");
-let index3 = path.join(__dirname, "views/about.html");
-let index2 = path.join(__dirname, "views/contact.html");
-let index4 = path.join(__dirname, "views/post.html");
+app.use(expressLayout);
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// let index1 = path.join(__dirname, "views/index.html");
+// let index3 = path.join(__dirname, "views/about.html");
+// let index2 = path.join(__dirname, "views/contact.html");
+// let index4 = path.join(__dirname, "views/post.html");
 
 app.get("/", (req, res) => {
-  res.sendFile(index1);
+  res.render("index", {
+    title: "Home page",
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(index3);
+  res.render("about", {
+    title: "About-us",
+  });
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(index2);
+  res.render("contact", {
+    title: "Contact-us",
+  });
 });
 
-app.get("/products", (req, res)=>{
-   res.sendFile(index4)
-})
+app.get("/products", (req, res) => {
+  res.render("post", {
+    title: "Products",
+  });
+});
 
-app.get("/api/names", (req, res) =>{
-   res.status(201).json({
-      success: 'success',
-      data,
-   })
-})
+app.post("/products", (req, res) => {
+  
+});
 
-
+app.get("/api/names", (req, res) => {
+  res.status(201).json({
+    success: "success",
+    data,
+  });
+});
 
 const PORT = process.env.PORT || 1100;
 
